b7519c4e5138b809edecad6b1cb4a50b08036bd3
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2026-03-22 19:22:12 -0300

    feat: add CountryInfoService SOAP client configuration

--- src/main/java/br/com/gustavoeguedes/soap_client/config/SoapConfig.java
+++ src/main/java/br/com/gustavoeguedes/soap_client/config/SoapConfig.java
@@ -2,6 +2,8 @@ package br.com.gustavoeguedes.soap_client.config;
 
 import com.dataaccess.webservicesserver.NumberConversion;
 import com.dataaccess.webservicesserver.NumberConversionSoapType;
+import org.oorsprong.websamples_countryinfo.CountryInfoService;
+import org.oorsprong.websamples_countryinfo.CountryInfoServiceSoapType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,4 +15,12 @@ public class SoapConfig {
         NumberConversion service = new NumberConversion();
         return service.getNumberConversionSoap();
     }
+
+    @Bean
+    public CountryInfoServiceSoapType countryInfoServiceSoapType() {
+        org.oorsprong.websamples_countryinfo.CountryInfoService service = new CountryInfoService();
+        return service.getCountryInfoServiceSoap();
+    }
+
+
 }
